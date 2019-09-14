ed5f7451fe71162bc323df26ea34119febb09be9
Valentine <46331217+erelin6613@users.noreply.github.com>
2019-09-14 20:24:35 +0300

    Update from 14-Sep-2019

--- img_rec.py
+++ img_rec.py
@@ -4,40 +4,94 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.preprocessing.image import load_img, ImageDataGenerator, img_to_array, array_to_img
-from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import preprocess_input, VGG16
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 
-path = '/home/val/google_recaptcha_set/recaptcha_set/3x3'
 
 
-categories = ['bus', 'traffic lights', 'crosswalks', 'bicycles',
-				'a fire hydrant', 'cars', 'chimneys',
-				'parking meters', 'palm trees', 'stairs', 'bridges']
+def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if (ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows + 1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, cols, i+1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i])
+        plt.show()
+        print('plots should have been run')
 
+
+pics_path = '/home/val/google_recaptcha_set/recaptcha_set/3x3/'
+
+categories = ['bus', 'traffic_lights', 'crosswalks', 'bicycles',
+        'fire_hydrant', 'cars', 'chimneys', 'stairs', 'bridges']
+
+labeled_data = {}
+
+for cat in categories:
+  pics = []
+  path = pics_path+cat
+#  label = cat
+  for root, dirs, files in os.walk(path):
+    for name in files:
+      pics.append(load_img(os.path.join(root, name)))
+  labeled_data[cat] = pics
+
+print(labeled_data)
+
+#test_path = '/home/val/google_recaptcha_set/recaptcha_set/3x3'
+
+batch_size = 4
+
+#X_train, X_test, y_train, y_test = train_test_split(images, labels)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Conv2D(32, (3, 3)))
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Conv2D(32, (3, 3)))
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Conv2D(64, (3, 3)))
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Dropout(0.5))
-model.add(Dense(len(categories)))
-model.add(Activation('sigmoid'))
+#model.add(Flatten())
+#model.add(Dense(64))
+#model.add(Dropout(0.5))
+#model.add(Dense(len(categories)))
+#model.add(Activation('sigmoid'))
 
+#print(VGG16().summary())
+#vgg16_model = VGG16()
 
+#for layer in vgg16_model.layers:
+  #print(layer)
+#  model.add(layer)
+
+#model.layers.pop()
+model.add(Dense(len(categories), activation='softmax'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit_generator(pics_generator, steps_per_epoch=4, epochs=6, verbose=1, validation_data=(X_test, y_test))
 
+print(model.summary())
+
+
+
+
+#conf_mat = confusion_matrix(test_labels, predected_labels)
+
+"""
 batch_size = 16
 train_datagen = ImageDataGenerator(rotation_range=0, width_shift_range=0.25,
 									height_shift_range=0.25, rescale=1./255, 
@@ -94,7 +148,7 @@ categories = ['bus', 'traffic lights', 'crosswalks', 'bicycles',
    	#	fin_img.save('/home/val/google_recaptcha_set/recaptcha_set/3x3/pic_{}.jpg'.format(j))
    		#fin_img.show()
    	#	j += 1
-   		#time.sleep(15)
+   		#time.sleep(15)"""
 
 
 
